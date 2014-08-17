e9b7775c617d24502f1344e026b095bf728046d0
ishandutta2007 <ishandutta2007@gmail.com>
2014-08-17 17:09:01 +0530

    Changing legend colour

--- BreakthroughsML.R
+++ BreakthroughsML.R
@@ -14,17 +14,8 @@ labels <- c("nosql",
             )
 v <- c()
 
-for(i in table(years))v<-c(v,1:i)
 
-data =data.frame( V1=years, V2=systems, disloc=v )
-dev.new()
-ggplot() +
-  geom_segment(aes(x = V1,y = disloc,xend = V1),data=data,yend = 0) +
-  geom_segment(aes(x = min(years),y = 0,xend = max(years),yend = 0),data=data,arrow = arrow(length = unit(x = 0.2,units = 'cm'),type = 'closed')) +
-  geom_text(aes(x = V1,y = disloc,label = V2,colour=factor(labels)),data=data,hjust = 1.0,vjust = 1.0,parse = FALSE,angle = 40.0) +
-  geom_point(aes(x = V1,y = disloc),data=data) +
-  scale_x_continuous(breaks = years,labels = years) +
-  opts(axis.text.x = theme_text(size = 12.0,angle = 90.0),axis.text.y = theme_blank(),axis.ticks = theme_blank(),axis.title.x = theme_blank(),axis.title.y = theme_blank())
+for(i in table(years))v<-c(v,1:i)
 
 all <-
 c(c("PCA",1986),
@@ -49,10 +40,12 @@ labels <- c(labels, rep("Languages",4))
 v <- c(v, c(0.25,0.25,0.65,0.45))
 data =data.frame( V1=years, V2=systems, disloc=v )
 dev.new()
+Palette1 <- c('red','green','blue','violet','black')
 ggplot() +
   geom_segment(aes(x = V1,y = disloc,xend = V1),data=data,yend = 0) +
   geom_segment(aes(x = min(years),y = 0,xend = max(years),yend = 0),data=data,arrow = arrow(length = unit(x = 0.2,units = 'cm'),type = 'closed')) +
   geom_text(aes(x = V1,y = disloc,label = V2,colour=factor(labels)),data=data,hjust = 1.0,vjust = 1.0,parse = FALSE,angle = 40.0) +
   geom_point(aes(x = V1,y = disloc),data=data) +
+  scale_colour_manual(values=Palette1 ) +
   #scale_x_continuous(breaks = years,labels = years) +
   opts(title="Breakthroughs in Machine Learning, Analytics and BigData", axis.text.x = theme_text(size = 12.0,angle = 90.0), axis.text.y = theme_blank(), axis.ticks = theme_blank(), axis.title.x = theme_blank(), axis.title.y = theme_blank())
