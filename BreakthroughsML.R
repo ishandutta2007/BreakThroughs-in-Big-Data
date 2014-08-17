91e1554e1133f7e6cfd9fc7c43ccc74218c97516
ishandutta2007 <ishandutta2007@gmail.com>
2014-08-17 17:27:17 +0530

    adding facebook's presto

--- BreakthroughsML.R
+++ BreakthroughsML.R
@@ -1,7 +1,7 @@
 require(ggplot2)
 require(grid)
-years <- c(2003,2004,2005,2006,2007,2007,2008,2008,2008,2009,2009,2010,2011,2011,2011,2012,2013)
-systems <- c("Memcached","MapReduce","CouchDB","BigTable/Hbase","MongoDB","Dynamo","Pig","HIVE","Cassandra","Voldemort","Riak","Dremel","Megastore","Tenzing","Spark/Shark","Spanner","Impala")
+years <- c(2003,2004,2005,2006,2007,2007,2008,2008,2008,2009,2009,2010,2011,2011,2011,2012,2013,2013)
+systems <- c("Memcached","MapReduce","CouchDB","BigTable/Hbase","MongoDB","Dynamo","Pig","HIVE","Cassandra","Voldemort","Riak","Dremel","Megastore","Tenzing","Spark/Shark","Spanner","Impala","Presto")
 
 labels <- c("nosql",
             "batch",
@@ -10,7 +10,7 @@ labels <- c("nosql",
             rep("nosql",3),
             "sql-like",
             "nosql",
-            rep("sql-like",4)
+            rep("sql-like",5)
             )
 v <- c()
 
@@ -41,7 +41,8 @@ v <- c(v, c(0.25,0.25,0.65,0.45))
 data =data.frame( V1=years, V2=systems, disloc=v )
 dev.new()
 Palette1 <- c('red','green','blue','violet','black')
-ggplot() +
+
+plot <- ggplot() +
   geom_segment(aes(x = V1,y = disloc,xend = V1),data=data,yend = 0) +
   geom_segment(aes(x = min(years),y = 0,xend = max(years),yend = 0),data=data,arrow = arrow(length = unit(x = 0.2,units = 'cm'),type = 'closed')) +
   geom_text(aes(x = V1,y = disloc,label = V2,colour=factor(labels)),data=data,hjust = 1.0,vjust = 1.0,parse = FALSE,angle = 40.0) +
@@ -49,3 +50,5 @@ ggplot() +
   scale_colour_manual(values=Palette1 ) +
   #scale_x_continuous(breaks = years,labels = years) +
   opts(title="Breakthroughs in Machine Learning, Analytics and BigData", axis.text.x = theme_text(size = 12.0,angle = 90.0), axis.text.y = theme_blank(), axis.ticks = theme_blank(), axis.title.x = theme_blank(), axis.title.y = theme_blank())
+plot
+ggsave(filename="C:\\Users\\DELL\\Documents\\GitHub\\BreakThroughs in Big Data\\BreakThroughsML.jpg", plot=plot)
